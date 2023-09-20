497fe9c2a3c6d5a958d93878a3e63ec0c2b222bb
Yasinda Samaranayake <yasindas@yahoo.com>
2023-09-20 16:56:18 +0530

    Added results page and UI changes

--- app.py
+++ app.py
@@ -11,15 +11,30 @@ def load_model():
 def predict(text):
     svm_model, vectorizer = load_model()
     text = vectorizer.transform([text])
-    return svm_model.predict(text)
+    prediction = svm_model.predict(text)
+    prediction_proba = svm_model.predict_proba(text)
+    return [prediction, prediction_proba]
+
+def get_sentiment(prediction):
+    if prediction == '1':
+        return 'Positive'
+    elif prediction == '0':
+        return 'Neutral'
+    else:
+        return 'Negative'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         user_text = request.form['text']
-        prediction = predict(user_text)
-        return str(prediction[0])
-    return render_template('index.html')
+        try:
+            prediction, proba = predict(user_text)
+            sentiment = get_sentiment(str(prediction[0]))
+            return render_template('result.html', user_text=user_text, sentiment=sentiment, proba=proba)
+        except:
+            return 'Something went wrong'
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
